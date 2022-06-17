877c462dec6aa077cd9dc5bb41cc9a0bb7e32a64
Henry Essinghigh <henry@essinghigh.dev>
2022-06-17 21:22:00 +0000

    Optimizations

--- nordvpn.py
+++ nordvpn.py
@@ -2,10 +2,8 @@ from nordvpn_connect import *
 import os
 import socket
 
-# Switzerland numero uno
 settings = initialize_vpn("Switzerland")
 
-# NordLynx Local IP = 10.5.0.2, so if this does not return that then something is wrong
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
@@ -18,21 +16,11 @@ def get_ip():
         s.close()
     return IP
 
-# This isn't really supposed to be used for intiial connection, but it works
-def connect():
-    rotate_VPN(settings)
-
-# Change as neccessary for OpenVPN Protocols
 def main():
     if (get_ip() != "10.5.0.2"):
-        print("Debug: Disconnected")
-        connect()
-        time.sleep(10) # Just in-case it takes NordVPN a little to connect
-    else:
-        print("Debug: Connected")
-    time.sleep(5)
-
+        rotate_VPN(settings)
 
 if __name__ == "__main__":
     while True:
         main()
+        time.sleep(5)
